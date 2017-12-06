421fcad222c1a47d9a86172f9204c5773d3f68d2
AllaBabikyan <alla.babikyan@polymorphic.io>
2017-12-06 15:12:10 +0400

    some bugs

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 	var colors = ["green", "yellow"];
 	$("#circle").click(function(){
 		var rand = Math.floor(Math.random() * colors.length);
-		$(this).css("background-color", colors[rand]);
-		
+		$(this).css("background-color", colors[rand]);		
 	});
 });
