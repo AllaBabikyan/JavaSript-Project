94cf236c5cccc960edf8a548e60e1d571ddca1a0
Alla Babikyan <alla.babikyan@polymorphic.io>
2017-12-06 12:49:36 +0400

    new project

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var colors = ["green", "yellow"];
+	$("#circle").click(function(){
+		var rand = Math.floor(Math.random() * colors.length);
+		$(this).css("background-color", colors[rand]);
+	});
+});
