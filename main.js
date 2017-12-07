0b90b4538f750e94bc613da2605bc8dd891a363f
Alla Babikyan <alla.babikyan@polymorphic.io>
2017-12-07 12:23:26 +0400

    removed bugs

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-	$(document).ready(function(){
-	var colors = ["yellow", "green"];
-	var i = 0;
-	var selectedColor;
-    $("#circle").click(function() {
-        selectedColor = colors[i];
-        document.getElementById("circle").style.backgroundColor = selectedColor;
-        i++;
-        if(i >= colors.length)
-            i = 0;
+        var colors = ["yellow", "green"];
+        var i = 0;
+        var selectedColor;
+        $("#circle").click(function() {
+            selectedColor = colors[i];
+            document.getElementById("circle").style.backgroundColor = selectedColor;
+            i++;
+            if(i >= colors.length)
+                i = 0;
+        });
     });
-});
-});
+
 
