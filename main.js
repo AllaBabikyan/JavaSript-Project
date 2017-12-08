5371dcb2598abb516276e5e28afe960eec1f3ba9
Alla Babikyan <alla.babikyan@polymorphic.io>
2017-12-08 14:58:04 +0400

    new version of changing colors

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 $(document).ready(function(){
-        var colors = ["yellow", "green"];
-        var i = 0;
-        var selectedColor;
-        $("#circle").click(function() {
-            selectedColor = colors[i];
-            document.getElementById("circle").style.backgroundColor = selectedColor;
-            i++;
-            if(i >= colors.length)
-                i = 0;
-        });
-    });
+    $("#circle").on("click", function() {
+        $(this).toggleClass('back-red');
+    });    
+});
 
 
